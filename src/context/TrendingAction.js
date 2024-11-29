601b96c9c06ebe8ce3aad30dd0dbbb6ef2ab50dd
Rishav Yadav <rishavyadav154@gmail.com>
2024-11-29 14:53:44 +0530

    CORS -5

--- src/context/TrendingAction.js
+++ src/context/TrendingAction.js
@@ -1,6 +1,8 @@
 export const movies = async () => {
   const response = await fetch(
-    `https://67497afab97b14289d6d50b6--endearing-biscochitos-506ba9.netlify.app/.netlify/functions/tmdb`
+    "https://67497afab97b14289d6d50b6--endearing-biscochitos-506ba9.netlify.app/.netlify/functions/tmdb"
   );
   console.log(response);
+  const data = await response.json();
+  console.log(data);
 };
